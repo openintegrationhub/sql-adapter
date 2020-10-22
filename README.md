5cbdb98f9dca29a7bad0275abb8b8669c8b4b8a8
jkopen <jknoop@wice.de>
2020-10-22 13:42:29 +0200

    Adding stub for writing data

--- README.md
+++ README.md
@@ -19,6 +19,9 @@ The following is an example for the minimal config for the SQL-Adapter step in t
   query: 'SELECT * FROM sometable WHERE FROM_UNIXTIME(timestamp) > CURDATE() - INTERVAL 1 DAY'
 }`
 
+This connector also supports custom mapping via JSONATA in the step-config ex:
+
+`customMapping: '{\'fullName\': data.title & \' \' & data.firstName & \' \' & data.lastName}'`
 
 ## Actions and triggers
 The **adapter** supports the following **actions** and **triggers**:
@@ -28,14 +31,33 @@ The **adapter** supports the following **actions** and **triggers**:
 
   All triggers are of type '*polling'* which means that the **trigger** will be scheduled to execute periodically.
 
-  It will send the provided SQL-QUERY to the connected database account and fetches the resulting data row by row. But will only send data that is new or changed since the previous execution. Then it will emit one message per object that is added or changed since the last polling interval.
+  It will send the provided SQL-QUERY to the connected database account and fetches the resulting data row by row. It is recommended to create a separate database user with the required permission.
+
+  ---
+  But will only send data that is new or changed since the previous execution. Then it will emit one message per object that is added or changed since the last polling interval.
 
   For this case at the very beginning we just create an empty `snapshot` object. Later on we attach ``lastUpdated`` to it. At the end the entire object should be emitted as the message body.
+  ---
 
 #### Actions:
-  - This connector does not have any actions yet
+  - Write Query to MySQL database (```addData.js```)
 
+  All placeholders in the query will be replaced with data if found.
 
+  Example:
+
+  Query:
+  `INSERT INTO address_company (email) VALUES ("{email}")`
+
+  payload:
+
+  `{ email: 'testmail@testmail.test' }`
+
+  Will result in:
+
+  `INSERT INTO address_company (email) VALUES ("testmail@testmail.test")`
+
+  This is not a prepared statement.
 ## License
 
 Apache-2.0 © [Wice GmbH](https://wice.de/)
--- test/utils.test.js
+++ test/utils.test.js
@@ -2,11 +2,11 @@
 
 const { expect } = require('chai');
 
-const { mysqlFetch } = require('./../lib/utils/mysql');
-const { mssqlFetch } = require('./../lib/utils/mssql');
-const { postgreFetch } = require('./../lib/utils/postgreSQL');
+const { mysqlFetch, mysqlExecute } = require('./../lib/utils/mysql');
+const { mssqlFetch, mssqlExecute } = require('./../lib/utils/mssql');
+const { postgreFetch, postgreExecute } = require('./../lib/utils/postgreSQL');
 
-describe('Utils', () => {
+describe('DB read', () => {
   before(async () => {
   });
 
@@ -73,13 +73,92 @@ describe('Utils', () => {
     const query = 'SELECT * FROM testtable';
 
     const results = [];
+    function handleMSSQL(data, last) {
+      // console.log('Data:', data);
+      results.push(data);
+      if (last) console.log('All done');
+    }
+
+    const result = await mssqlFetch(databaseConfig, query, handleMSSQL, true);
+    // console.log('Result:', result);
+    expect(result).to.equal(true);
+  });
+});
+
+describe('DB write', () => {
+  before(async () => {
+  });
+
+  it('should only test on local', async () => {
+    expect(true).to.equal(true);
+  });
+
+  it.skip('should write to mysql DB', async () => {
+    const databaseConfig = {
+      databaseType: 'MySQL',
+      user: 'root',
+      password: '',
+      databaseUrl: 'localhost',
+      port: '3306',
+      databaseName: 'wice',
+    };
+    const query = 'INSERT INTO address_company (email) VALUES ("testmail@testmail.test")';
+
+    const results = [];
+    function handleMySQL(data, last) {
+      // console.log('Data:', data);
+      results.push(data);
+      if (last) console.log('All done');
+    }
+
+    const result = await mysqlExecute(databaseConfig, query, handleMySQL, true);
+    // console.log('Result:', result);
+    expect(result).to.equal(true);
+  });
+
+  it.skip('should write to PostgreSQL DB', async () => {
+    const databaseConfig = {
+      databaseType: 'PostgreSQL',
+      user: 'postgres',
+      password: 'myPassword',
+      databaseUrl: 'localhost',
+      port: '5432',
+      databaseName: 'testdb',
+    };
+    const query = 'INSERT INTO testtable ("type", "color", "location", "install_date") VALUES ("slideX", "red", "south", "2020-10-28")';
+
+    const results = [];
     function handlePostgreSQL(data, last) {
       // console.log('Data:', data);
       results.push(data);
       if (last) console.log('All done');
     }
 
-    const result = await mssqlFetch(databaseConfig, query, handlePostgreSQL, true);
+    const result = await postgreExecute(databaseConfig, query, handlePostgreSQL, true);
+    // console.log('Result:', result);
+    expect(result).to.equal(true);
+  });
+
+
+  it.skip('should insert into MS SQL database', async () => {
+    const databaseConfig = {
+      databaseType: 'SQL',
+      user: 'sa',
+      password: 'P@55w0rd',
+      databaseUrl: 'localhost',
+      port: '1433',
+      databaseName: 'testdb',
+    };
+    const query = 'INSERT INTO testtable (userId, title) VALUES (77, "Sometitle")';
+
+    const results = [];
+    function handleMSSQL(data, last) {
+      // console.log('Data:', data);
+      results.push(data);
+      if (last) console.log('All done');
+    }
+
+    const result = await mssqlExecute(databaseConfig, query, handleMSSQL, true);
     // console.log('Result:', result);
     expect(result).to.equal(true);
   });
